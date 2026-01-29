c369ea9e355f5aecbb123c5e089cb74ce9a4cca7
HenriqueGBA <gabgba90@gmail.com>
2026-01-29 15:13:03 -0300

    adjust categoria model and create dbcontext with dtos

---
+++ BackendCGR/Data/ApplicationDbContext.cs.cs
@@ -0,0 +1,44 @@
+using BackendCGR.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleGastosResidenciais.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Transacao> Transacoes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pessoa>(builder =>
+            {
+                builder.Property(p => p.Nome).HasMaxLength(200).IsRequired();
+            });
+
+            modelBuilder.Entity<Categoria>(builder =>
+            {
+                builder.Property(c => c.Descricao).HasMaxLength(400).IsRequired();
+                builder.Property(c => c.Finalidade).IsRequired();
+            });
+
+            modelBuilder.Entity<Transacao>(builder =>
+            {
+                builder.Property(t => t.Descricao).HasMaxLength(400).IsRequired();
+                builder.Property(t => t.Valor).IsRequired();
+                builder.Property(t => t.Tipo).IsRequired();
+                builder.HasOne(t => t.Categoria)
+                    .WithMany(c => c.Transacoes)
+                    .HasForeignKey(t => t.CategoriaId);
+                builder.HasOne(t => t.Pessoa)
+                    .WithMany(p => p.Transacoes)
+                    .HasForeignKey(t => t.PessoaId);
+            });
+        }
+    }
+}
